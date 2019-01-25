ec2bf1701af7564d89ae06a9c5304bcc5d248925
Jesse Omoefe <jesseefe@gmail.com>
2019-01-25 11:20:49 +0100

    feat(design/template): Link pages together
    -navigate from one page to the other
    
    [delivers #163473551]

--- UI/js/create.js
+++ UI/js/create.js
@@ -1,4 +1,22 @@
-const menuIcon =  document.querySelector('#form-area > i').onclick = function(){
+// const menuIcon =  document.querySelector('#form-area > i').onclick = function(){
+//     let menu = document.querySelector('#side-bar');
+//     menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+// }
+
+const closeMenu =  document.querySelector('#form-area > .fa-times');
+closeMenu.style.display = 'none';
+const showMenu =  document.querySelector('#form-area > .fa-bars');
+showMenu.onclick = function(){
+    this.style.display = 'none';
     let menu = document.querySelector('#side-bar');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
+    closeMenu.style.display = 'block';
+    menu.style.display = 'block'; 
+};
+closeMenu.onclick = function(){
+    this.style.display = 'none';
+    showMenu.style.display = 'block';
+    let menu = document.querySelector('#side-bar');
+    const closeMenu =  document.querySelector('#form-area > .fa-times');
+    closeMenu.style.display = 'none';
+     menu.style.display = 'none'; 
+}; 
--- UI/js/offices.js
+++ UI/js/offices.js
@@ -1,9 +1,14 @@
 function showCandidates(){
     let clickedButton = this;
     let parent = clickedButton.parentElement;
-    clickedButton.innerHTML = clickedButton.innerHTML === 'Hide candidates' ? 'See all candidates' : 'Hide candidates';
+    if(clickedButton.innerHTML === 'Hide candidates' || clickedButton.innerHTML === 'See all candidates'){
+        clickedButton.innerHTML = clickedButton.innerHTML === 'Hide candidates' ? 'See all candidates' : 'Hide candidates';
+    } else {
+        clickedButton.innerHTML = clickedButton.innerHTML === 'Show result' ? 'Hide result' : 'Show result';
+    }
     parent.nextElementSibling.style.display = parent.nextElementSibling.style.display === 'none' ? 'block' : 'none';
-}  
+}
+
 const candidates = document.getElementsByClassName('candidates');
 const showCandidatesButton = document.getElementsByClassName('show-candidates');
 
@@ -12,9 +17,22 @@ for (let i = 0; i < candidates.length; i++) {
 }
 for (let i = 0; i < showCandidatesButton.length; i++) {
     showCandidatesButton[i].addEventListener('click', showCandidates);
-}   
- 
-const menuIcon =  document.querySelector('#offices > i').onclick = function(){
-    let menu = document.querySelector('#side-bar');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
+
+const closeMenu =  document.querySelector('#offices > .fa-times');
+closeMenu.style.display = 'none';
+const showMenu =  document.querySelector('#offices > .fa-bars');
+showMenu.onclick = function(){
+    this.style.display = 'none';
+    let menu = document.querySelector('#side-bar');
+    closeMenu.style.display = 'block';
+    menu.style.display = 'block'; 
+};
+closeMenu.onclick = function(){
+    this.style.display = 'none';
+    showMenu.style.display = 'block';
+    let menu = document.querySelector('#side-bar');
+    const closeMenu =  document.querySelector('#offices > .fa-times');
+    closeMenu.style.display = 'none';
+     menu.style.display = 'none'; 
+};   
